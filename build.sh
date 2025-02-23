7150320e50492bf614c561f118e711c7053253c6
Midnight Salmon <197456194+Midnight-Salmon@users.noreply.github.com>
2025-02-24 10:45:53 +1300

    Removed optimisation flag causing incorrect behaviour.

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ gcc \
   recce-mission.c \
   -Wl,-Bstatic -lwinpthread -Wl,-Bdynamic \
   -lws2_32 \
-  -Wall -Wextra -std=c99 -pedantic -O2 \
+  -Wall -Wextra -std=c99 -pedantic \
   -pthread \
   -o reccem.exe
